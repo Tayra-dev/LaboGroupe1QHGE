3c377ca376facd07068bc81f0e4ef38514ad90ce
q-bckrt <pom.schc@egmail.com>
2026-07-31 12:07:50 +0200

    added relationships (back_populates) to tickets, categories and priorities.

--- helpdesk/app/models/priority.py
+++ helpdesk/app/models/priority.py
@@ -5,10 +5,14 @@ class Priority(BaseEntity, db.Model):
 
     __tablename__ = "priorities"
 
+    # Base Columns
     priority_id = db.Column("priorityid", db.Integer, primary_key=True)
     name = db.Column("priorityname", db.String(255), nullable=False, index=True, unique=True)
     level = db.Column("prioritylevel", db.Integer, nullable=False)
     delay_hours = db.Column("prioritydelayhours", db.Integer, nullable=False)
+    
+    # Relationships
+    tickets = db.relationship("Ticket", back_populates="priority")
     
     def __repr__(self):
         return f"<Priority ['{self.name}' ID: {self.priority_id}]>"
