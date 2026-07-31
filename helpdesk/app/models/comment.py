4bdacef5ba9bdab74602a2abf0006240f47d45e0
manuvdb <vanderbruggen.emmanuel@gmail.com>
2026-07-31 11:21:13 +0200

    Feat: change of nomenclature for 'Comment'

--- helpdesk/app/models/comment.py
+++ helpdesk/app/models/comment.py
@@ -5,17 +5,17 @@ class Comment(BaseEntity, db.Model):
 
     """Commentaire publié sur un ticket."""
 
-    __tablename__ = 'comments'
+    __tablename__ = "comments"
 
-    comment_id = db.Column(db.Integer, primary_key = True, autoincrement=True)
-    comment_content = db.Column(db.Text, nullable=False)
-    author_id = db.Column(db.Integer, db.ForeignKey('users.user_id'), nullable=False)
-    ticket_id = db.Column(db.Integer, db.ForeignKey('tickets.ticket_id'), nullable=False)
+    comment_id = db.Column("commentid", db.Integer, primary_key = True, autoincrement=True)
+    comment_content = db.Column("commentcontent", db.Text, nullable=False)
+    author_id = db.Column("authorid", db.Integer, db.ForeignKey('users.userid'), nullable=False)
+    ticket_id = db.Column("ticketid", db.Integer, db.ForeignKey('tickets.ticketid'), nullable=False)
 
     #Relations
 
-    author = db.relationship('User', back_populates='comments')
-    ticket = db.relationship('Ticket', back_populates='comments')
+    author = db.relationship("User", back_populates="comments")
+    ticket = db.relationship("Ticket", back_populates="comments")
 
     def __repr__(self):
-        return f"<Comment {self.comment_id}>"
+        return f"<Comment {self.comment_id}: {self.comment_content}>"
