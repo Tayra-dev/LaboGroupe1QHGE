35b2c625aacc0ac54f61f146c253bef4cd22d947
Gillian <gillian.delvigne@gmail.com>
2026-07-31 23:19:27 +0200

    [ADD] mappers: implement UserMapper with methods for converting between User entity and UserDTO, and handling user registration form data

---
+++ helpdesk/app/forms/user_register_form.py
@@ -0,0 +1,41 @@
+from flask_wtf import FlaskForm
+from wtforms import EmailField, PasswordField, StringField
+from wtforms.validators import DataRequired, Email, EqualTo, Length, Regexp
+
+from app import app
+
+PASSWORD_VALIDATORS = (
+    [
+        DataRequired(),
+        Length(4, 25),
+        EqualTo("confirm", message="Les mots de passe encodés ne correspondent pas"),
+    ]
+    if app.debug
+    else [
+        DataRequired(),
+        Length(12, 80),
+        Regexp(
+            r"(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!?(){}*$&@+=])",
+            message="Le mot de passe doit contenir au moins une lettre minuscule, une majuscule, un chiffre et un caractère spécial : !?(){}*$&@+=",
+        ),
+    ]
+)
+
+
+class UserRegisterForm(FlaskForm):
+    name = StringField(
+        "Nom d'utilisateur", validators=[DataRequired(), Length(min=2, max=50)]
+    )
+    email = EmailField(
+        "Email",
+        validators=[
+            DataRequired(),
+            Email("L'adresse email est requise pour l'inscription"),
+        ],
+    )
+    password = PasswordField("Mot de passe", validators=PASSWORD_VALIDATORS)
+    confirm = PasswordField("Confirmation du mot de passe")
+    firstname = StringField(
+        "Prénom", validators=[DataRequired(), Length(min=2, max=50)]
+    )
+    lastname = StringField("Prénom", validators=[DataRequired(), Length(min=2, max=50)])
