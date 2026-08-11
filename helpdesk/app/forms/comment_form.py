9506afebffcf8ad02c2d6ed73da8e4a674d1efd9
manuvdb <vanderbruggen.emmanuel@gmail.com>
2026-08-11 11:27:14 +0200

    Fix comment_form : supprime les ID car intégrés via attributs de la class dans mapper

--- helpdesk/app/forms/comment_form.py
+++ helpdesk/app/forms/comment_form.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import HiddenField, TextAreaField
+from wtforms import TextAreaField
 from wtforms.validators import DataRequired
 
 
@@ -11,19 +11,5 @@ class CommentForm(FlaskForm):
         "Commentaire",
         validators=[
             DataRequired(message="Le contenu du commentaire est obligatoire !")
-        ]
-    )
-
-    # Champs cachés (Hidden) : invisibles pour l'utilisateur, mais lus par le Mapper
-    author_id = HiddenField(
-        "ID auteur",
-        validators=[
-            DataRequired(message="L'auteur est obligatoire !")
-        ]
-    )
-    ticket_id = HiddenField(
-        "ID ticket",
-        validators=[
-            DataRequired(message="Le ticket est obligatoire !")
         ]
     )
