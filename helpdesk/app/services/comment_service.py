bde72101ad24a793947d5e146116b222f3a491f2
manuvdb <vanderbruggen.emmanuel@gmail.com>
2026-08-13 09:35:25 +0200

    fix: Update import statement for AbstractService to AbstractAuthService in CommentService

--- helpdesk/app/services/comment_service.py
+++ helpdesk/app/services/comment_service.py
@@ -1,6 +1,6 @@
 from app import db
 from app import app
-from app.framework.service.abstract_service import AbstractService
+from app.framework.service.abstract_auth_service import AbstractAuthService
 from app.models.comment import Comment
 from app.mappers.comment_mapper import CommentMapper
 from app.framework.decorators.injectable import injectable
