8f0865e9fac7fe559003ce8e024da6048bb97509
manuvdb <vanderbruggen.emmanuel@gmail.com>
2026-08-20 16:01:21 +0200

    [ADD] enhance attachment handling by saving file data and updating entity mapping

--- helpdesk/app/mappers/attachment_mapper.py
+++ helpdesk/app/mappers/attachment_mapper.py
@@ -12,12 +12,13 @@ class AttachmentMapper(AbstractMapper):
         return AttachmentDTO.build_from_entity(attachment)
 
     @staticmethod
-    def form_to_entity(form, attachment: Attachment, author_id: int, ticket_id: int) -> Attachment:
+    def form_to_entity(form, attachment: Attachment, author_id: int, ticket_id: int, attachment_path: str) -> Attachment:
         if isinstance(form, AttachmentForm):
             file_data = form.attachment.data
 
             if file_data:
                 attachment.attachment_filename = file_data.filename 
+                attachment.attachment_path = attachment_path
 
                 file_data.seek(0, os.SEEK_END)
                 attachment.attachment_size = file_data.tell()
