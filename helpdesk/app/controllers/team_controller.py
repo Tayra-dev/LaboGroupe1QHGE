66cee6a5e27a61eb0798506d635b970ad422aa8d
Gillian <gillian.delvigne@gmail.com>
2026-08-24 13:38:12 +0200

    [ADD] Implement team service methods (find, update, delete)

--- helpdesk/app/controllers/team_controller.py
+++ helpdesk/app/controllers/team_controller.py
@@ -1,4 +1,4 @@
-from flask import redirect, render_template
+from flask import redirect, render_template, flash
 
 from app import app
 from app.framework.decorators.inject import inject
@@ -16,11 +16,16 @@ def team_list(team_service: TeamService):
 
 
 @app.route("/teams/add", methods=["GET", "POST"])
+@app.route("/teams/<int:team_id>/edit", methods=["GET", "POST"])
 @auth_required(role_name="ADMIN")
 @inject
 def create_team(team_service: TeamService, user_service: UserService):
     form = TeamCreationForm()
-    users = [user for user in user_service.find_all() if (user.has_role("TECHNICIEN") and user.team_id is None)]
+    users = [
+        user
+        for user in user_service.find_all()
+        if (user.has_role("TECHNICIEN") and user.team_id is None)
+    ]
     users_by_id = {user.user_id: user for user in users}
 
     form.members.choices = [
@@ -31,7 +36,9 @@ def create_team(team_service: TeamService, user_service: UserService):
 
     if form.validate_on_submit():
         app.logger.info(f"form sent successfully: ${form.members.data}")
-        team_service.insert(form, user_service)
+        team = team_service.insert(form, user_service)
+        if team is not None:
+            flash("L'équipe {team.name} a été créée correctement.", "success")
     return render_template(
         "teams/add_or_update.html", form=form, team=None, members_data=members_data
     )
--- helpdesk/app/services/team_service.py
+++ helpdesk/app/services/team_service.py
@@ -1,6 +1,7 @@
 from app import app, db
 from sqlalchemy.exc import SQLAlchemyError
 from app.framework.decorators.injectable import injectable
+from app.framework.decorators.inject import inject
 from app.framework.injector import Scope
 from app.framework.service.abstract_service import AbstractService
 from app.models.team import Team
@@ -12,6 +13,10 @@ from app.services.user_service import UserService
 
 @injectable(scope=Scope.SCOPED)
 class TeamService(AbstractService):
+    @inject
+    def __init__(self, user_service: UserService):
+        self.__user_service = user_service
+
     def find_all(self) -> list[TeamDTO]:
         try:
             teams = Team.query.filter_by(active=True).order_by(Team.team_id).all()
@@ -22,30 +27,80 @@ class TeamService(AbstractService):
         else:
             return [TeamMapper.entity_to_dto(team) for team in teams]
 
-    def find_one_entity(self, entity_id: int):
-        pass
+    def find_one_entity(self, team_id: int):
+        try:
+            return db.session.get(Team, team_id)
+        except SQLAlchemyError as e:
+            db.session.rollback()
+            app.logger.error(
+                f"Erreur lors de la récupération de l'équipe, id {team_id}: {e}"
+            )
+            return None
 
     def find_one(self, team_id: int) -> TeamDTO:
-        pass
+        try:
+            team = db.dession.get(Team, team_id)
+            return TeamMapper.entity_to_dto(team) if team is not None else None
+        except SQLAlchemyError as e:
+            db.session.rollback()
+            app.logger.error(
+                f"Erreur lors de la récupération de l'équipe, id {team_id}: {e}"
+            )
+            return None
 
     def find_one_by(self, **kwargs) -> TeamDTO:
-        pass
+        try:
+            team = Team.query.filter_by(**kwargs).first()
+            return TeamMapper.entity_to_dto(team) if team is not None else None
+        except SQLAlchemyError as e:
+            db.session.rollback()
+            app.logger.error(
+                f"Erreur lors de la récupération de l'équipe (DTO) avec {[f'{k} = {v}' for k, v in kwargs.items()]} : {e}"
+            )
+            return None
 
-    def insert(
-        self, form: TeamCreationForm, user_service: UserService
-    ) -> TeamDTO | None:
+    def insert(self, form: TeamCreationForm) -> TeamDTO | None:
         try:
             team = Team()
             TeamMapper.form_to_entity(form, team)
-            team.members = user_service.find_entities_by_ids(form.members.data)
+            team.members = self.__user_service.find_entities_by_ids(form.members.data)
             db.session.add(team)
             db.session.commit()
         except SQLAlchemyError as e:
             db.session.rollback()
             app.logger.error(f"Erreur lors de la création de l'équipe: {e}")
+            return None
+        else:
+            return TeamMapper.entity_to_dto(team)
 
     def update(self, team_id: int, data) -> TeamDTO | None:
-        pass
+        try:
+            team = self.find_one_entity(team_id)
+            if team is None:
+                return None
+            TeamMapper.form_to_entity(data, team)
+            db.session.commit()
+        except SQLAlchemyError as e:
+            db.session.rollback()
+            app.logger.error(
+                f"Erreur lors de la mise à jour de l'équipe {team_id}: {e}"
+            )
+            return None
+        else:
+            return TeamMapper.entity_to_dto(team)
 
     def delete(self, team_id: int) -> int | None:
-        pass
+        try:
+            team = self.find_one_entity(team_id)
+            if team is None:
+                return None
+            team.soft_delete()
+            db.session.commit()
+        except SQLAlchemyError as e:
+            db.session.rollback()
+            app.logger.error(
+                f"Erreur lors de la suppression de l'équipe {team_id}: {e}"
+            )
+            return None
+        else:
+            return team_id
