b2820bc78eeaee800caf11b95bc5c1e33b6d8ce8
q-bckrt <pom.schc@egmail.com>
2026-08-27 13:12:56 +0200

    [IMP] Only admin can now create categories and priorities

--- helpdesk/app/controllers/category_controller.py
+++ helpdesk/app/controllers/category_controller.py
@@ -1,11 +1,13 @@
+from app.framework.decorators.auth_required import auth_required
 from app.framework.decorators.inject import inject
 from flask import render_template
-from app import app, csrf
+from app import app
 from app.forms.categories.category_form import CategoryForm
 from app.services.category_service import CategoryService
 
-@inject
 @app.route("/categories/create", methods=["GET", "POST"])
+@auth_required("ADMIN")
+@inject
 def create_category(
     category_service: CategoryService
 ):
