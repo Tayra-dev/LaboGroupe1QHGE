aa52eba9b6144a3f9744d08dbdafdb008be27eb1
q-bckrt <pom.schc@egmail.com>
2026-08-27 13:12:56 +0200

    [IMP] Only admin can now create categories and priorities

--- helpdesk/app/controllers/priority_controller.py
+++ helpdesk/app/controllers/priority_controller.py
@@ -1,7 +1,7 @@
-from app.framework.decorators import auth_required
+from app.framework.decorators.auth_required import auth_required
 from app.framework.decorators.inject import inject
 from flask import render_template
-from app import app, csrf
+from app import app
 from app.forms.priorities.priority_form import PriorityForm
 from app.services.priority_service import PriorityService
 
